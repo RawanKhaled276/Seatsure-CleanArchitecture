29c4a858bb1fed9f0fb21c16b007a25a21b7ed39
Abdullah Ahmed <abdallahsherdy@gmail.com>
2026-08-08 22:44:46 +0300

    implementing repositories

---
+++ Seatsure.DAL/Repositories/Interfaces/ITicketTypeRepository.cs
@@ -0,0 +1,11 @@
+using Seatsure.Domain;
+
+namespace Seatsure.DAL.Repositories.Interfaces;
+
+public interface ITicketTypeRepository
+{
+    Task<TicketType?> GetByIdAsync(Guid id);
+    Task<IEnumerable<TicketType>> GetByEventIdAsync(Guid eventId);
+    Task AddAsync(TicketType ticketType);
+    Task SaveChangesAsync();
+}
---
+++ Seatsure.DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+using Seatsure.Domain;
+
+namespace Seatsure.DAL.Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User?> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+    Task SaveChangesAsync();
+}
